4f8bb5864fd022aec00b78e71434c883488641e4
SahinCagri <asdcagriasd@gmail.com>
2020-12-19 22:45:43 +0300

    characterController

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -17,40 +17,56 @@ public class Character : MonoBehaviour
     private Touch touch;
     private float zRot;
     private bool canJump;
+    private bool isStationary;
+    private bool isBegan;
     private void Update()
     {
+        CalculateForceAccordingToAngle();
         if (Input.touchCount > 0 && canJump)
         {
             FollowSledOnXAxis();
-
             touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
-                transform.SetParent(null);
-                rb.gravityScale = 1;
-                rb.AddForce(jumpHeight, ForceMode2D.Impulse);
+                isBegan = true;
             }
             if (touch.phase == TouchPhase.Stationary)
             {
-                RotateCharacter();
-                
+                isStationary = true;
             }
             if (touch.phase == TouchPhase.Ended)
             {
-                 canJump = false;
+                canJump = false;
+                isStationary = false;
             }
         }
         else
         {
-            if (Mathf.Abs(sled.position.y - transform.position.y) < 2f)
+            if (Mathf.Abs(sled.position.y - transform.position.y) < 1)
             {
                 FollowSled();
             }
-            else {
+            else
+            {
                 FollowSledOnXAxis();
             }
         }
 
+    }
+
+    private void FixedUpdate()
+    {
+        if (isStationary)
+        {
+            if (isBegan)
+            {
+                rb.velocity = Vector3.zero;
+                rb.gravityScale = 1;
+                rb.AddForce(jumpHeight, ForceMode2D.Impulse);
+                isBegan = false;
+            }
+            RotateCharacter();
+        }
     }
 
     private void RotateCharacter()
@@ -62,15 +78,24 @@ public class Character : MonoBehaviour
     private void FollowSled()
     {
         rb.gravityScale = 0;
-        canJump=true;
-        transform.SetParent(sled);
-        transform.position = Vector3.zero;
-        transform.rotation = Quaternion.identity;
+        canJump = true;
+        transform.position = sled.position;
+        transform.rotation = sled.rotation;
     }
-    private void FollowSledOnXAxis(){
-        Vector2 tempPos=transform.position;
+    private void FollowSledOnXAxis()
+    {
+        Vector2 tempPos = transform.position;
         tempPos.x = sled.position.x;
         transform.position = tempPos;
     }
-
+    private void CalculateForceAccordingToAngle(){
+        float zAngle = sled.eulerAngles.z;
+        print(zAngle);
+        if(zAngle < 330 && zAngle > 300){
+            jumpHeight.y =2;
+        }
+        else{
+            jumpHeight.y = 10;
+        }
+    }
 }
